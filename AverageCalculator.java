043aaee7711a41024bf74e3211e984f1fda6ba90
Vincent Langat <104572853+VincentLangat033@users.noreply.github.com>
2023-07-26 20:24:27 +0300

    [ADD] Identification of bug in assignment 4 done

---
+++ AverageCalculator.java
@@ -0,0 +1,20 @@
+public class AverageCalculator {
+    public static double calculateAverage(int[] numbers) {
+        int total = 0;
+        for (int num : numbers) {
+            total += num;
+        }
+        double average = (double) total / numbers.length;
+        /*
+The possible error in the code above is caused by  ( total / numbers.length) integer division
+To fix this error, the division is done using floating point division
+*/
+        return average;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {5, 10, 15, 20, 37};
+        double result = calculateAverage(data);
+        System.out.println("The average is: " + result);
+    }
+}
